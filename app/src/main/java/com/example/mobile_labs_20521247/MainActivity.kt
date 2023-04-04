a8e8270cfe39a66360a436ce50f178ff007cc16d
Duy - UIT <88521480+thaiduy132@users.noreply.github.com>
2023-04-04 22:35:55 +0700

    Finished Lab 1

--- app/src/main/java/com/example/mobile_labs_20521247/MainActivity.kt
+++ app/src/main/java/com/example/mobile_labs_20521247/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mobile_labs_20521247
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mobile_labs_20521247.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +13,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
 
     }
